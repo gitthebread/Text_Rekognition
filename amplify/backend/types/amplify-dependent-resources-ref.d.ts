79e0a237c64c3feadc05378aaaf6ee453017670b
gitthebread <112457612+gitthebread@users.noreply.github.com>
2023-04-11 18:49:52 +0700

    .

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "textrekognition1972fb83": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "S3FriendlyName": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- app/src/main/java/com/example/text_rekognition_aws/MainActivity.java
+++ app/src/main/java/com/example/text_rekognition_aws/MainActivity.java
@@ -1,30 +1,123 @@
 package com.example.text_rekognition_aws;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+import java.io.File;
+
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.S3Object;
+import com.amazonaws.services.rekognition.model.DetectTextRequest;
+import com.amazonaws.services.rekognition.model.DetectTextResult;
+import com.amazonaws.services.rekognition.model.TextDetection;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_select_image;
+    Button btn_select_image, btn_get_texts;
+    ImageView img_view;
+    TextView txt_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_select_image.findViewById(R.id.btn_select_image);
+        btn_select_image = findViewById(R.id.btn_select_image);
+        img_view = findViewById(R.id.img_view);
+        txt_view = findViewById(R.id.txt_view);
+        btn_get_texts = findViewById(R.id.btn_get_texts);
 
-        btn_select_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
+        initializeAmplify();
+
+        btn_select_image.setOnClickListener(view -> {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 10);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
         });
+
+        btn_get_texts.setOnClickListener(view -> {
+            AmazonRekognition rekognitionClient = new AmazonRekognitionClient();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && resultCode == RESULT_OK){
+
+            img_view.setImageURI(data.getData());
+
+            Uri selectedImageUri = data.getData();
+
+            File imageFile = new File(getRealPathFromURI(selectedImageUri));
+
+            uploadFile(imageFile);
+        }
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+    private void initializeAmplify(){
+        try {
+            // Add these lines to add the AWSCognitoAuthPlugin and AWSS3StoragePlugin plugins
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("Text_Rekognition_AWS", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("Text_Rekognition_AWS", "Could not initialize Amplify", error);
+        }
+    }
+
+    private void uploadFile(File selectedImagePath) {
+        Amplify.Storage.uploadFile(
+                "my-photo.jpg",
+                selectedImagePath,
+                result -> Log.i("Text_Rekognition_AWS", "Successfully uploaded: " + result.getKey()),
+                storageFailure -> Log.e("Text_Rekognition_AWS", "Upload failed", storageFailure)
+        );
     }
 }
